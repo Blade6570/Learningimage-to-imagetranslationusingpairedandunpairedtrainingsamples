13d3c8741270db770ceffa3b6edf18b207d4b4c3
Soumya Tripathy <tripathy@mahti-login1.mahti.csc.fi>
2021-05-25 11:49:59 +0300

    May be fix for the issue in the visdom as in
    https://github.com/Blade6570/Learningimage-to-imagetranslationusingpairedandunpairedtrainingsamples/issues/6

--- util/visualizer.py
+++ util/visualizer.py
@@ -100,8 +100,9 @@ class Visualizer():
     def plot_current_errors(self, epoch, counter_ratio, opt, errors):
         if not hasattr(self, 'plot_data'):
             self.plot_data = {'X': [], 'Y': [], 'legend': list(errors.keys())}
-        self.plot_data['X'].append(epoch + counter_ratio)
-        self.plot_data['Y'].append([errors[k] for k in self.plot_data['legend']])
+        self.plot_data['X'].append(float(epoch + counter_ratio))
+        self.plot_data['Y'].append([float(errors[k]) for k in self.plot_data['legend']])
+        import pdb; pdb.set_trace()
         self.vis.line(
             X=np.stack([np.array(self.plot_data['X'])] * len(self.plot_data['legend']), 1),
             Y=np.array(self.plot_data['Y']),
